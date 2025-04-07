89bc3dcfce6de5af71ae918edc126bcec35728ac
kimminu7 <rock9024@naver.com>
2025-04-07 16:44:50 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/org/example/snsprojcet/SnsProjcetApplication.java
@@ -0,0 +1,13 @@
+package org.example.snsprojcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsProjcetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsProjcetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/snsprojcet/SnsProjcetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.snsprojcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsProjcetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
