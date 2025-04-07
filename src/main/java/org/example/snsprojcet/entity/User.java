6343b2771e9b5c4fb480edc8c1b4ad1dbd95d195
chohyeokjun <chj3731@naver.com>
2025-04-07 17:20:04 +0900

    JPA 의존성 추가

--- src/main/java/org/example/snsprojcet/entity/User.java
+++ src/main/java/org/example/snsprojcet/entity/User.java
@@ -1,4 +1,47 @@
 package org.example.snsprojcet.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "user")
+@Getter
 public class User {
+
+    // 기본키
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 이름
+    @Column(nullable = false)
+    private String name;
+
+    // 나이
+    @Column(nullable = false)
+    private Long age;
+
+    // 닉네임
+    @Column(nullable = false)
+    private String nickname;
+
+    // email
+    @Column(nullable = false)
+    private String email;
+
+    // 비밀번호
+    @Column(nullable = false)
+    private String password;
+
+    // 기본 생성자
+    public User(){}
+
+    public User (String name, Long age, String nickname, String email, String password) {
+        this.name = name;
+        this.age = age;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
 }
