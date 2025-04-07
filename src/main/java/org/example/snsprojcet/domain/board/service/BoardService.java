5f22555cdac8d232ef0521d358c2c88effa68867
kimminu7 <rock9024@naver.com>
2025-04-07 21:07:40 +0900

    refactor : 주석 작업

--- src/main/java/org/example/snsprojcet/domain/board/service/BoardService.java
+++ src/main/java/org/example/snsprojcet/domain/board/service/BoardService.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 public interface BoardService {
 
-    // 일정 생성
+    // 게시판 생성
     BoardResponseDto createBoard(String title, String nickName, String contents);
-
+    // 게시판 전체 조회
     List<BoardResponseDto> findAll();
-
+    // 게시판 단건 조회
     BoardResponseDto findById(Long id);
-
+    // 게시판 수정
     String updateBoard(Long id, String title, String contents);
-
+    // 게시판 삭제
     String deleteBoard(Long id);
 }
