ef8abf1896ba39d8a3f40536867ddbdfee2d9f3f
todok0317 <todok0317@naver.com>
2025-04-08 12:00:47 +0900

    댓글 기능 구현

---
+++ src/main/java/org/example/snsprojcet/dto/CommentResponseDto.java
@@ -0,0 +1,20 @@
+package org.example.snsprojcet.dto;
+
+import lombok.Getter;
+import org.example.snsprojcet.entity.Comment;
+
+@Getter
+public class CommentResponseDto {
+
+    private final Long id;
+    private final String content;
+
+    public CommentResponseDto(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public static CommentResponseDto toDto (Comment comment) {
+        return new CommentResponseDto(comment.getId(), comment.getContent());
+    }
+}
---
+++ src/main/java/org/example/snsprojcet/dto/UpdateCommentRequestDto.java
@@ -0,0 +1,4 @@
+package org.example.snsprojcet.dto;
+
+public class UpdateCommentRequestDto {
+}
