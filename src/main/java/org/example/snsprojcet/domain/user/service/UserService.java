6db9044768ac6fd859842a3706a6e7ceced5aceb
chohyeokjun <chj3731@naver.com>
2025-04-09 21:06:27 +0900

    refactor: 비밀번호 형식 유효성 검사에 특수문자 추가 검사

--- src/main/java/org/example/snsprojcet/domain/user/service/UserService.java
+++ src/main/java/org/example/snsprojcet/domain/user/service/UserService.java
@@ -77,7 +77,7 @@ public class UserService {
 
     // 비밀번호 형식 유효성 검사
     private boolean isValidPasswordFormat(String password) {
-        String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";   // 최소 8자, 하나 이상의 숫자, 하나 이상의 영문자 포함
+        String regex = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*\\W).{8,}$";   // 비밀번호는 대소문자 포함한 영문 + 숫자 + 특수문자를 최소 1글자씩 포함하며 최소 8글자 이상이어야 합니다
         return password != null && password.matches(regex);
 
     }
